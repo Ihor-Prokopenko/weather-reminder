31900fd9ae9ed85fcc1f6bcc9c79f43b4c334b72
SWAGhettie <swaghettiemonsta@gmail.com>
2023-07-24 23:14:29 +0300

    added nginx

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -16,14 +16,3 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
